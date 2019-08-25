1e53efa9576763bc1145b8baa4bae17e8fdcdbb9
Zac Smith <zrs3141592@gmail.com>
2019-08-25 02:21:35 -0600

    Create component and add props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./App.css";
 
-const App = () => {
-  console.log("hello from component");
+const Hello = props => {
   return (
     <div>
-      <p>Hello world</p>
+      <h1>Hello {props.name}</h1>
+    </div>
+  );
+};
+
+const App = () => {
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  return (
+    <div>
+      <p>Greetings, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <Hello name={"Zac"} />
     </div>
   );
 };
