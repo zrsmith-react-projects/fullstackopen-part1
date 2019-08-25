f0daa394cf3624c4e68e42fb63cbb6c0904203a3
Zac Smith <zrs3141592@gmail.com>
2019-08-25 02:30:01 -0600

    last commit before starting 1.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./App.css";
 
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        This is a footer, Copyright <a href="#">Stuff</a>{" "}
+      </p>
+    </div>
+  );
+};
+
 const Hello = props => {
   return (
     <div>
-      <h1>Hello {props.name}</h1>
+      <h1>
+        Hello {props.name}, your number is {props.number}
+      </h1>
     </div>
   );
 };
@@ -19,7 +31,9 @@ const App = () => {
       <p>
         {a} plus {b} is {a + b}
       </p>
-      <Hello name={"Zac"} />
+      <Hello name={"Zac"} number={a} />
+      <Hello name={"Brittany"} number={b} />
+      <Footer />
     </div>
   );
 };
